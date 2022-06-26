8baf77541d49d9aa58bfc2b2297f3a5aec0e903e
GodsonNtungi <godsonezekiel31@gmail.com>
2022-06-26 11:56:56 +0300

    movie spawn

---
+++ main.py
@@ -0,0 +1,84 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+from dotenv import load_dotenv
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import telebot
+import requests
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+chrome_options = Options()
+chrome_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
+
+def access_Web():
+    driver = webdriver.Chrome(service=Service("chromedriver.exe"), options=chrome_options)
+    driver.get('https://hdtoday.tv/home')
+
+    content = driver.page_source
+    soup = BeautifulSoup(content, features="html.parser")
+    app = soup.find('div', attrs={'id': 'app'})
+    wrapper = app.find('div', attrs={'id': 'wrapper'})
+    main_wrapper = wrapper.find('div', attrs={'id': 'main-wrapper'})
+    container = main_wrapper.find('div', attrs={'class': 'container'})
+    section = container.find('section', attrs={'class': 'block_area block_area_home section-id-01'})
+    tab = section.find('div', attrs={'class': 'tab-content'})
+    return tab
+
+
+@bot.message_handler(commands=['Movies'])
+def Movies(message):
+    tab = access_Web()
+    trending_M = tab.find('div', attrs={'id': 'trending-movies'})
+    block = trending_M.find('div', attrs={'class': 'block_area-content block_area-list film_list film_list-grid'})
+    f_wrap = block.find('div', attrs={'class': 'film_list-wrap'})
+    i = 0
+    for a in f_wrap.find_all('div', attrs={'class': 'flw-item'}):
+
+        poster = a.find('div', attrs={'class': 'film-poster'})
+        quality = poster.find('div', attrs={'class': 'pick film-poster-quality'})
+        the_one = poster.find('a')
+        images = poster.find('img')
+        print(images['data-src'])
+        img = requests.get(images['data-src'])
+
+        print(the_one['title'] + ' ' + quality.text)
+        bot.send_message(message.chat.id, the_one['title'] + ' ' + quality.text)
+        bot.send_photo(message.chat.id, img.content)
+        i = i + 1
+        if i > 10:
+            break
+
+
+@bot.message_handler(commands=['Series'])
+def Series(message):
+    tab = access_Web()
+
+    trending_S = tab.find('div', attrs={'id': 'trending-tv'})
+    block = trending_S.find('div', attrs={'class': 'block_area-content block_area-list film_list film_list-grid'})
+    f_wrap = block.find('div', attrs={'class': 'film_list-wrap'})
+
+    i = 0
+    for a in f_wrap.find_all('div', attrs={'class': 'flw-item'}):
+
+        poster = a.find('div', attrs={'class': 'film-poster'})
+        quality = poster.find('div', attrs={'class': 'pick film-poster-quality'})
+        the_one = poster.find('a')
+        images = poster.find('img')
+        print(images['data-src'])
+        img = requests.get(images['data-src'])
+
+        print(the_one['title'] + ' ' + quality.text)
+        # telegram_send.send(messages=[the_one['title'] + ' ' + quality.text], images=[img.content])
+        bot.send_message(message.chat.id, the_one['title'] + ' ' + quality.text)
+        bot.send_photo(message.chat.id, img.content)
+        i = i + 1
+        if i > 10:
+            break
+
+
+bot.polling()
